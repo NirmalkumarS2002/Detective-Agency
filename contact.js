de076c2a834f8470c585a297cbf1e7e8dcbab6a1
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-07-31 16:54:06 +0530

    task completed

--- contact.js
+++ contact.js
@@ -8,6 +8,8 @@ const messageInput = document.querySelector("#c-area");
 
 const errorMsgs = document.querySelectorAll(".errormsg");
 
+let submsg = document.querySelector(".submsg")
+
 //=========================
 // Restrict Name
 //=========================
@@ -26,8 +28,8 @@ numberInput.addEventListener("input", () => {
     numberInput.value = numberInput.value.replace(/[^0-9]/g, "");
 
     // Maximum 10 digits
-    if(numberInput.value.length > 10){
-        numberInput.value = numberInput.value.slice(0,10);
+    if (numberInput.value.length > 10) {
+        numberInput.value = numberInput.value.slice(0, 10);
     }
 
 });
@@ -36,11 +38,11 @@ numberInput.addEventListener("input", () => {
 // Validation
 //=========================
 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
 
     e.preventDefault();
 
-    errorMsgs.forEach(msg=>msg.textContent="");
+    errorMsgs.forEach(msg => msg.textContent = "");
 
     let valid = true;
 
@@ -49,72 +51,74 @@ form.addEventListener("submit",(e)=>{
 
     // Name
 
-    if(nameInput.value.trim()==""){
+    if (nameInput.value.trim() == "") {
 
-        errorMsgs[0].textContent="Enter your name";
-        valid=false;
+        errorMsgs[0].textContent = "Enter your name";
+        valid = false;
 
     }
-    else if(!nameRegex.test(nameInput.value.trim())){
+    else if (!nameRegex.test(nameInput.value.trim())) {
 
-        errorMsgs[0].textContent="Only letters are allowed";
-        valid=false;
+        errorMsgs[0].textContent = "Only letters are allowed";
+        valid = false;
 
     }
 
     // Email
 
-    if(emailInput.value.trim()==""){
+    if (emailInput.value.trim() == "") {
 
-        errorMsgs[1].textContent="Enter your email";
-        valid=false;
+        errorMsgs[1].textContent = "Enter your email";
+        valid = false;
 
     }
-    else if(!emailRegex.test(emailInput.value.trim())){
+    else if (!emailRegex.test(emailInput.value.trim())) {
 
-        errorMsgs[1].textContent="Enter a valid email";
-        valid=false;
+        errorMsgs[1].textContent = "Enter a valid email";
+        valid = false;
 
     }
 
     // Phone
 
-    if(numberInput.value.trim()==""){
+    if (numberInput.value.trim() == "") {
 
-        errorMsgs[2].textContent="Enter your phone number";
-        valid=false;
+        errorMsgs[2].textContent = "Enter your phone number";
+        valid = false;
 
     }
-    else if(numberInput.value.length!=10){
+    else if (numberInput.value.length != 10) {
 
-        errorMsgs[2].textContent="Enter a valid 10-digit number";
-        valid=false;
+        errorMsgs[2].textContent = "Enter a valid 10-digit number";
+        valid = false;
 
     }
 
     // Purpose
 
-    if(purposeInput.value.trim()==""){
+    if (purposeInput.value.trim() == "") {
 
-        errorMsgs[3].textContent="Enter the purpose";
-        valid=false;
+        errorMsgs[3].textContent = "Enter the purpose";
+        valid = false;
 
     }
 
     // Message
 
-    if(messageInput.value.trim()==""){
+    if (messageInput.value.trim() == "") {
 
-        errorMsgs[4].textContent="Please describe your case";
-        valid=false;
+        errorMsgs[4].textContent = "Please describe your case";
+        valid = false;
 
     }
 
     // Success
 
-    if(valid){
-
-        alert("Message Sent Successfully!");
+    if (valid) {
+         submsg.style.display="block"
+        setTimeout(() => {
+            submsg.style.display="none"
+        }, 2000);
 
         form.reset();
 
